7c6bf8303cfccfb043f27f442e273d3c22a7b761
Paola-Santanna <paolinhasj@gmail.com>
2024-05-07 09:31:23 -0300

    Add project files.

---
+++ Micro editor de texto - Arquivo/Program.cs
@@ -0,0 +1,6 @@
+﻿//Aula 14 - Tratamento de Arquivos
+
+string path = @"C:\Dados\"; //O @ demonstra que o que está estrito tem/é um caractere especial; aqui será escrito o caminho do arquivo
+string file = "arquivo.txt"; //nome e extensão do arquvo
+
+StreamWriter sw = new(); //Tem que passar como parâmetro o caminho e o nome do arquivo
