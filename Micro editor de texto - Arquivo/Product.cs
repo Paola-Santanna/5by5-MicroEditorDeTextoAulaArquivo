1af1b218910b042747086b31e5764af6b10eca91
Paola-Santanna <paolinhasj@gmail.com>
2024-05-07 14:59:51 -0300

    #Aula
    Desenvolvido na aula

---
+++ Micro editor de texto - Arquivo/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Micro_editor_de_texto___Arquivo
+{
+    internal class Product
+    {
+        /*
+         * Declaração das propriedades, com o get e o set
+         */
+        public int id { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+
+
+        /*
+         * No método construtor tem as informações das propriedades;
+         */
+        public Product(int id, string description, double price, int quantity)
+        {
+            this.id = id;
+            this.description = description;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public override string? ToString()
+        {
+            return id + "; " + description + "; " + price + "; " + quantity;
+        }
+    }
+}
